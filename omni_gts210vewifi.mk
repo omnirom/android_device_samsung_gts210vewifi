797904040c818c4cec195c73e586e10122dd9a89
Menno van Grinsven <scanno71@gmail.com>
2017-08-21 12:43:50 +0200

    gts210vewifi: Fix include dependencies
    Change-Id: I6df33943a092b297094c2c5376c5db8455d46e2d

--- omni_gts210vewifi.mk
+++ omni_gts210vewifi.mk
@@ -1,8 +1,38 @@
-$(call inherit-product, device/samsung/gts210vewifi/full_gts210vewifi.mk)
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Inherit some common Lineage stuff.
+#
+# This file is the build configuration for a full Android
+# build for grouper hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps).
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit some common OmniROM stuff.
 $(call inherit-product, vendor/omni/config/common_tablet.mk)
 
+# Inherit from gts210vewifi device
+$(call inherit-product, device/samsung/gts210vewifi/device.mk)
+
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_gts210vewifi
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
